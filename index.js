f9afb54df6705ec21a6e46039fefa5fc77e78278
SofianeAmeziane <67568893+SofianeAmeziane@users.noreply.github.com>
2021-04-01 15:25:28 -0400

    first commit from sandbox

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const express = require('express');
+const express = require("express");
 const server = express();
-const {ConnectionDb} = require('./DB/Connection');
+const { ConnectionDb } = require("./DB/Connection");
 
 async function runserver() {
-  
-await ConnectionDb();
-const portfoliorouter = require('./routes/portfolios');
-const PORT = parseInt(process.env.PORT, 10) || 3001 ;
+  await ConnectionDb();
+  const portfoliorouter = require("./routes/portfolios");
+  const PORT = parseInt(process.env.PORT, 10) || 3001;
 
-server.use(express.json());
-server.use('/api/v1/portfolios', portfoliorouter);
-server.listen(PORT, (err) => {
-if(err) console.log(err);
-console.log("port ready on :", PORT);
-})
-};
+  server.use(express.json());
+  server.use("/api/v1/portfolios", portfoliorouter);
+  server.listen(PORT, (err) => {
+    if (err) console.log(err);
+    console.log("port ready on :", PORT);
+  });
+}
 runserver();
-
-
+//comment
